9da5f4a3cb7e59b3c5f47aa1f2f3d58aa1986ec0
muyiwa_oni <muyiwa_oni@outlook.com>
2023-09-26 01:46:32 +0100

    Added Module VPC

--- variables.tf
+++ variables.tf
@@ -4,7 +4,7 @@ variable "region" {
   type = string
 }
 
-Variable "project_name" {
+variable "project_name" {
   description = "Project name."
   type = string
 }
@@ -13,3 +13,43 @@ variable "environment" {
   description = "Region to create resources."
   type = string
 }
+
+# vpc variable
+variable "vpc_cidr" {
+  description = "VPC CIDR block."
+  type = string
+}
+
+variable "public_subnet_cidr_az1" {
+  description = "Public subnet AZ1 CIDR."
+  type        = string
+}
+
+variable "public_subnet_cidr_az2" {
+  description = "Public Subnet AZ2 CIDR."
+  type        = string
+}
+
+variable "private_app_subnet_cidr_az1" {
+  description = "Private App Subnet AZ1 CIDR."
+  type        = string
+}
+
+variable "private_app_subnet_cidr_az2" {
+  description = "Private App Subnet AZ2 CIDR."
+  type        = string
+}
+
+variable "private_db_subnet_cidr_az1" {
+  description = "Private Database Subnet AZ1 CIDR."
+  type        = string
+}
+
+variable "private_db_subnet_cidr_az2" {
+  description = "Private Database Subnet AZ2 CIDR."
+  type        = string
+}
+
+
+
+
